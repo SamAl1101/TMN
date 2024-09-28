d69c8b583d58a883602b6b4eab983363249efc3c
Sam <samoleks@gmail.com>
2024-09-28 12:56:51 +0200

    first commit

---
+++ HelloApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿{
+    Console.Write("Write your name: ");
+    var name = Console.ReadLine();
+    Console.WriteLine($"Hello {name}!");
+}
+{
+    Console.WriteLine();
+}
+
+{
+    string name = "Sam";
+    Console.WriteLine($"Your name is {name}.");
+    name = "Nick";
+    Console.WriteLine($"My name is {name}. \tLet's build a society that will...");
+}
+
+{
+    const string NAME = "Bob";
+    Console.Write(NAME);
+    Console.Write(NAME);
+}
+
+{
+    Console.WriteLine();
+}
+
+{
+    Console.Write('\x22');      //ASCI code HEX 22 = "
+}
+
+Console.WriteLine('\u0550');      //UNICODE
